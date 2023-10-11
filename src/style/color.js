70dfa4d95383c8adf1a06c8c9982be9f0131d246
kimgyuha <kimgyuha25@gmail.com>
2023-10-11 14:43:30 +0900

    SET :: color, text 세팅

---
+++ src/style/color.js
@@ -0,0 +1,20 @@
+export const color = {
+  Basic: {
+    black: "#000000",
+    gray950: "#0D0D0D",
+    gray900: "#1A1A1A",
+    gray800: "#333333",
+    gray700: "#4D4D4D",
+    gray600: "#666666",
+    gray500: "#808080",
+    gray400: "#999999",
+    gray300: "#B3B3B3",
+    gray200: "#CCCCCC",
+    gray100: "#E6E6E6",
+    gray50: "#F2F2F2",
+    white: "#FFFFFF",
+  },
+  Brand: {
+    primary: "#F6869C",
+  },
+};
---
+++ src/style/text.js
@@ -0,0 +1,68 @@
+import { css } from "styled-components";
+
+export const text = {
+  display: {
+    d1: css`
+      font-size: 72px;
+      line-height: 120%;
+    `,
+    d2: css`
+      font-size: 64px;
+      line-height: 120%;
+    `,
+    d3: css`
+      font-size: 56px;
+      line-height: 120%;
+    `,
+  },
+  heading: {
+    h1: css`
+      font-size: 48px;
+      line-height: 150%;
+    `,
+    h2: css`
+      font-size: 44px;
+      line-height: 150%;
+    `,
+    h3: css`
+      font-size: 40px;
+      line-height: 150%;
+    `,
+    h4: css`
+      font-size: 36px;
+      line-height: 150%;
+    `,
+    h5: css`
+      font-size: 32px;
+      line-height: 150%;
+    `,
+  },
+  body: {
+    body1: css`
+      font-size: 28px;
+      line-height: 150%;
+    `,
+    body2: css`
+      font-size: 24px;
+      line-height: 150%;
+    `,
+    body3: css`
+      font-size: 20px;
+      line-height: 150%;
+    `,
+  },
+  paragraph: {
+    p1: css`
+      font-size: 16px;
+      line-height: 150%;
+    `,
+    p2: css`
+      font-size: 14px;
+      line-height: 150%;
+    `,
+    p3: css`
+      font-size: 12px;
+      line-height: 150%;
+    `,
+  },
+};
