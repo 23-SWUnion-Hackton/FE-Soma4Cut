fdf8a9220ad7a8472dc039b35fd745333ed09b7c
kimgyuha <kimgyuha25@gmail.com>
2023-10-11 14:55:38 +0900

    CHORE : text 사용법 수정

--- src/style/text.js
+++ src/style/text.js
@@ -1,66 +1,66 @@
-import { css } from "styled-components";
+import styled from "styled-components";
 
 export const text = {
   display: {
-    d1: css`
+    d1: styled.p`
       font-size: 72px;
       line-height: 120%;
     `,
-    d2: css`
+    d2: styled.p`
       font-size: 64px;
       line-height: 120%;
     `,
-    d3: css`
+    d3: styled.p`
       font-size: 56px;
       line-height: 120%;
     `,
   },
   heading: {
-    h1: css`
+    h1: styled.p`
       font-size: 48px;
       line-height: 150%;
     `,
-    h2: css`
+    h2: styled.p`
       font-size: 44px;
       line-height: 150%;
     `,
-    h3: css`
+    h3: styled.p`
       font-size: 40px;
       line-height: 150%;
     `,
-    h4: css`
+    h4: styled.p`
       font-size: 36px;
       line-height: 150%;
     `,
-    h5: css`
+    h5: styled.p`
       font-size: 32px;
       line-height: 150%;
     `,
   },
   body: {
-    body1: css`
+    body1: styled.p`
       font-size: 28px;
       line-height: 150%;
     `,
-    body2: css`
+    body2: styled.p`
       font-size: 24px;
       line-height: 150%;
     `,
-    body3: css`
+    body3: styled.p`
       font-size: 20px;
       line-height: 150%;
     `,
   },
   paragraph: {
-    p1: css`
+    p1: styled.p`
       font-size: 16px;
       line-height: 150%;
     `,
-    p2: css`
+    p2: styled.p`
       font-size: 14px;
       line-height: 150%;
     `,
-    p3: css`
+    p3: styled.p`
       font-size: 12px;
       line-height: 150%;
     `,
