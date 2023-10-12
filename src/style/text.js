a8effe15d3d375f0ba7c742df6e053bfddb38c34
sebanimm <gks32114221@gmail.com>
2023-10-12 09:20:11 +0900

    ADD : text에 font-weight 속성 추가

--- src/style/text.js
+++ src/style/text.js
@@ -3,78 +3,95 @@ import styled from "styled-components";
 export const text = {
   display: {
     d1: styled.p`
+      font-weight: 700;
       font-size: 72px;
       line-height: 120%;
     `,
     d2: styled.p`
+      font-weight: 700;
       font-size: 64px;
       line-height: 120%;
     `,
     d3: styled.p`
+      font-weight: 700;
       font-size: 56px;
       line-height: 120%;
     `,
   },
   heading: {
     h1: styled.p`
+      font-weight: 600;
       font-size: 48px;
       line-height: 150%;
     `,
     h2: styled.p`
+      font-weight: 600;
       font-size: 44px;
       line-height: 150%;
     `,
     h3: styled.p`
+      font-weight: 600;
       font-size: 40px;
       line-height: 150%;
     `,
     h4: styled.p`
+      font-weight: 600;
       font-size: 36px;
       line-height: 150%;
     `,
     h5: styled.p`
+      font-weight: 600;
       font-size: 32px;
       line-height: 150%;
     `,
   },
   body: {
     body1: styled.p`
+      font-weight: 500;
       font-size: 28px;
       line-height: 150%;
     `,
     body2: styled.p`
+      font-weight: 500;
       font-size: 24px;
       line-height: 150%;
     `,
     body3: styled.p`
+      font-weight: 500;
       font-size: 20px;
       line-height: 150%;
     `,
   },
   paragraph: {
     p1: styled.p`
+      font-weight: 400;
       font-size: 16px;
       line-height: 150%;
     `,
     p2: styled.p`
+      font-weight: 400;
       font-size: 14px;
       line-height: 150%;
     `,
     p3: styled.p`
+      font-weight: 400;
       font-size: 12px;
       line-height: 150%;
     `,
   },
   button: {
     button1: styled.p`
+      font-weight: 600;
       font-size: 24px;
       line-height: 120%;
     `,
     button2: styled.p`
+      font-weight: 600;
       font-size: 20px;
       line-height: 120%;
     `,
     button3: styled.p`
+      font-weight: 600;
       font-size: 16px;
       line-height: 120%;
     `,
